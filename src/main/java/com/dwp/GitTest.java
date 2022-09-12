e367a489153aa7b4980dc97205fd4b551ce81a4d
DWP <duweipeng_2020@qq.com>
2022-09-12 15:28:40 +0800

    push commit

--- src/main/java/com/dwp/GitTest.java
+++ src/main/java/com/dwp/GitTest.java
@@ -2,11 +2,7 @@ package com.dwp;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("push test");
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("master test");
